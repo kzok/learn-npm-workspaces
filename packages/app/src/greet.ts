7cc6bff852affc6d4428adc7802fd2923ea4b0b9
kzok <yes.notyet@gmail.com>
2022-11-03 18:19:10 +0900

    replace JavaScript with TypeScript

---
+++ packages/app/src/greet.ts
@@ -0,0 +1,3 @@
+import {greet} from "@example/greet/index";
+
+export const run = (): void => console.log(greet("world"));
--- packages/app/src/main.mjs
+++
@@ -1,3 +0,0 @@
-import {greet} from "@example/greet";
-
-console.log(greet("world"));
