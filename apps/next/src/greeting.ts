85f62fe5e3f4f1516394702238cfe28d4a562286
kzok <yes.notyet@gmail.com>
2022-11-05 15:09:24 +0900

    npm run fmt

--- apps/next/src/greeting.ts
+++ apps/next/src/greeting.ts
@@ -1,3 +1,3 @@
-import {greet} from "@example/greet";
+import { greet } from "@example/greet";
 
 export const greeting = greet("Next.js");
--- apps/next/src/pages/index.tsx
+++ apps/next/src/pages/index.tsx
@@ -1,8 +1,8 @@
-import type {NextPage} from "next";
-import {greeting} from "@/greeting";
+import type { NextPage } from "next";
+import { greeting } from "@/greeting";
 
 const HomePage: NextPage = () => {
-  return <h1>{greeting}</h1>
-}
+  return <h1>{greeting}</h1>;
+};
 
 export default HomePage;
