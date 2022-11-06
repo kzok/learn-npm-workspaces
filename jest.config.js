24c46c8d92d47c211cca22f35f3b0415e5ce6fcb
kzok <yes.notyet@gmail.com>
2022-11-06 14:36:11 +0900

    move packages jest config into project root

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  projects: ["<rootDir>/packages/*"],
+  projects: [
+    {
+      cacheDirectory: `<rootDir>/tmp/jest/packages`,
+      testMatch: [`<rootDir>/packages/**/__tests__/?(*.)test.[jt]s?(x)`],
+      transform: {
+        "\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.base.json" }],
+      },
+    },
+  ],
 };
